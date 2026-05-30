d8be069296349deb38fc5c45eb7fc7a18e9b8454
muschellij2 <muschellij2@gmail.com>
2026-05-30 13:27:52 -0400

    added the docs again

--- R/actiread-reexports.R
+++ R/actiread-reexports.R
@@ -1,64 +1,78 @@
-#' @noRd
+#' @inherit actiread::acti_convert_sensorlogger_time
 #' @importFrom actiread acti_convert_sensorlogger_time
 #' @export
 acti_convert_sensorlogger_time <- actiread::acti_convert_sensorlogger_time
 
-#' @noRd
+#' Example SensorLog file path.
+#'
+#' Re-exported from `actiread`.
 #' @importFrom actiread acti_example_sensorlog_file
 #' @export
 acti_example_sensorlog_file <- actiread::acti_example_sensorlog_file
 
-#' @noRd
+#' Example SensorLogger file path.
+#'
+#' Re-exported from `actiread`.
 #' @importFrom actiread acti_example_sensorlogger_file
 #' @export
 acti_example_sensorlogger_file <- actiread::acti_example_sensorlogger_file
 
-#' @noRd
+#' Example SensorLogger location file path.
+#'
+#' Re-exported from `actiread`.
 #' @importFrom actiread acti_example_sensorlogger_location_file
 #' @export
 acti_example_sensorlogger_location_file <- actiread::acti_example_sensorlogger_location_file
 
-#' @noRd
+#' @inherit actiread::acti_read_sensorlog
 #' @importFrom actiread acti_read_sensorlog
 #' @export
 acti_read_sensorlog <- actiread::acti_read_sensorlog
 
-#' @noRd
+#' @inherit actiread::acti_read_sensorlogger
 #' @importFrom actiread acti_read_sensorlogger
 #' @export
 acti_read_sensorlogger <- actiread::acti_read_sensorlogger
 
-#' @noRd
+#' @inherit actiread::acti_read_sensorlogger_general
 #' @importFrom actiread acti_read_sensorlogger_general
 #' @export
 acti_read_sensorlogger_general <- actiread::acti_read_sensorlogger_general
 
-#' @noRd
+#' @inherit actiread::acti_read_sensorlogger_location
 #' @importFrom actiread acti_read_sensorlogger_location
 #' @export
 acti_read_sensorlogger_location <- actiread::acti_read_sensorlogger_location
 
-#' @noRd
+#' @inherit actiread::acti_rewrite_sensorlog_csv
 #' @importFrom actiread acti_rewrite_sensorlog_csv
 #' @export
 acti_rewrite_sensorlog_csv <- actiread::acti_rewrite_sensorlog_csv
 
-#' @noRd
+#' SensorLog CSV column-name mapping.
+#'
+#' Re-exported from `actiread`.
 #' @importFrom actiread acti_sensorlog_csv_colnames_mapping
 #' @export
 acti_sensorlog_csv_colnames_mapping <- actiread::acti_sensorlog_csv_colnames_mapping
 
-#' @noRd
+#' SensorLog CSV parsing specification.
+#'
+#' Re-exported from `actiread`.
 #' @importFrom actiread acti_sensorlog_csv_spec
 #' @export
 acti_sensorlog_csv_spec <- actiread::acti_sensorlog_csv_spec
 
-#' @noRd
+#' SensorLogger location CSV column-name mapping.
+#'
+#' Re-exported from `actiread`.
 #' @importFrom actiread acti_sensorlogger_location_colnames_mapping
 #' @export
 acti_sensorlogger_location_colnames_mapping <- actiread::acti_sensorlogger_location_colnames_mapping
 
-#' @noRd
+#' SensorLogger location CSV parsing specification.
+#'
+#' Re-exported from `actiread`.
 #' @importFrom actiread acti_sensorlogger_location_spec
 #' @export
 acti_sensorlogger_location_spec <- actiread::acti_sensorlogger_location_spec
